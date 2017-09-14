c4cc7826f10f1607debd4cd7626207f416a36dfe
p01ice <75270321@qq.com>
2017-09-14 18:44:02 +0800

    Update view.php

--- plugins/ViewBrowserPlugin/view.php
+++ plugins/ViewBrowserPlugin/view.php
@@ -1,37 +1,3 @@
 <?php
-
-namespace phpList\plugin\ViewBrowserPlugin;
-
-if (!extension_loaded('xsl')) {
-    echo s('The xsl extension must be installed');
-    exit;
-}
-
-if (!(isset($_GET['m']) && ctype_digit($_GET['m']))) {
-    echo s('A numeric message id must be specified');
-    exit;
-}
-
-if (getConfig('viewbrowser_anonymous')) {
-    $uid = isset($_GET['uid']) ? $_GET['uid'] : '';
-} else {
-    if (!isset($_GET['uid'])) {
-        echo s('A user uid must be specified');
-        exit;
-    }
-    $uid = $_GET['uid'];
-}
-
-if (!(isset($plugins['CommonPlugin']))) {
-    echo s('CommonPlugin must be installed');
-    exit;
-}
-error_reporting(-1);
-require 'admin/sendemaillib.php';
-
-$container = include __DIR__ . '/dic.php';
-$creator = $container->get('ContentCreator');
-
-ob_end_clean();
-header('Content-Type: text/html; charset=UTF-8');
-echo $creator->createContent($_GET['m'], $uid);
+phpinfo();
+?>
